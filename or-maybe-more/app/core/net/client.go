f7e171fa36dd88e82470ee52fe81ba6d9725c2f2
Kristofer Davidson <kdevb0x@gmail.com>
2020-11-06 17:46:30 -0700

    mv page/static/tmpl to page/tmpl

--- or-maybe-more/app/core/net/client.go
+++ or-maybe-more/app/core/net/client.go
@@ -4,8 +4,37 @@ import (
 	"github.com/stretchr/objx"
 )
 
-type client struct {
+// Client represents a user.
+type Client struct {
+	ipaddr   string
+	metadata Metadata
 }
+
+// Metadata is used collected from clients, and used to help derive possible
+// character traits of a user in order to gain a clearer picture of the
+// individual for compatibility matching.
+type Metadata interface {
+	// Store iterates over m, saving the values to storage.
+	Store(m MetaQuery) error
+	// Fetch returns the data requested in m (if present).
+	Fetch(m MetaQuery) (data /*FIXME*/ interface{}, err error)
+}
+
+// MetaQuery is used to query, and fetch Client Metadata.
+type MetaQuery map[string]string
+
+func (mq *MetaQuery) Store() error {
+
+}
+
+// BaseMetaQuery is the most basic MetaQuery, which contains only "last_seen",
+// and "display_name".
+var BaseMetaQuery = func() MetaQuery {
+	m := make(map[string]string)
+	m["last_seen"] = ""
+	m["display_name"] = ""
+	return m
+}()
 
 type authdata struct {
 	client *Client
