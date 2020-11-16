ab0595dc38a164e0fa757541c8dbba78414501b6
Kristofer Davidson <kdevb0x@gmail.com>
2020-11-15 21:24:58 -0700

    started writing oauth handlers; remove redundent file

--- or-maybe-more/app/client.go
+++ or-maybe-more/app/client.go
@@ -1,4 +1,4 @@
-package client
+package app
 
 import (
 	"crypto/ed25519"
@@ -9,42 +9,44 @@ import (
 	"golang.org/x/crypto/nacl/auth"
 )
 
-type clientID struct {
+type ClientID struct {
 	dna         ed25519.PrivateKey
 	displayName string
+	key         ed25519.PublicKey
 }
 
-// An clientID is constructed by initializing its DNA.
-func initDNA() *clientID {
-	_, privKey, err := ed25519.GenerateKey(nil)
+// An ClientID is constructed by initializing its DNA.
+func initClientDNA() *ClientID {
+	pubKey, privKey, err := ed25519.GenerateKey(nil)
 	if err != nil {
 		panic(fmt.Errorf("failed to generate keys: %w\n", err).Error())
 	}
-	return &clientID{dna: privKey}
+	return &ClientID{dna: privKey, key: pubKey}
 }
 
-// Verify that the message msg has been signed by this clientID.
-func (id *clientID) Verify(msg []byte) (bool, error) {
-	if id.dna == nil {
+// Verify that the message msg has been signed by this ClientID.
+func (id *ClientID) Verify(msg []byte) (ok bool, err error) {
+	if (id.dna == nil || len(id.dna) == 0) || (id.key == nil || len(id.key) == 0) {
 		return false, errors.New(fmt.Sprintf("DNA not initialized for %s\n", id.displayName))
 	}
-	ok := auth.Verify(msg, nil, privKeyToBP(id.dna))
+	ok = auth.Verify(id.key, msg, privKeyToBPtr(id.dna))
 	if !ok {
-		return false, errors.New("Verification failed")
+		err = errors.New("Verification failed")
+		return ok, err
 	}
 	return true, nil
 }
 
-// Sign the message msg using this clientID.
-func (id *clientID) Sign(msg []byte) (sig []byte, err error) {
+// Sign the message msg using this ClientID.
+func (id *ClientID) Sign(msg []byte) (sig []byte, err error) {
 	if len(id.dna) == 0 {
 		return nil, errors.New("error: the dna of this identidy has not been initialized")
 	}
 	return ed25519.Sign(id.dna, msg), nil
 }
 
-// Converts a byte slice to a byte array pointer.
-func bToBP(b []byte) *[32]byte {
+// Converts b[:31] to a 32 byte array pointer.
+func bptr(b []byte) *[32]byte {
 	var s *[32]byte
 	// since k uses s as the underlying array, we this should work.
 	k := s[:]
@@ -53,40 +55,57 @@ func bToBP(b []byte) *[32]byte {
 }
 
 // Converts from ed25519.PrivateKey to a byte array pointer.
-func privKeyToBP(k ed25519.PrivateKey) *[32]byte {
+func privKeyToBPtr(k ed25519.PrivateKey) *[32]byte {
 	b := []byte(k)
-	return bToBP(b)
+	return bptr(b)
 }
 
 // Client represents a user.
 type Client struct {
 	ipaddr   string
-	metadata Metadata
+	Metadata Metadata
+	id       *ClientID
 }
 
-// Metadata is used collected from clients, and used to help derive possible
+// Metadata is collected from Clients, and used to help derive possible
 // character traits of a user in order to gain a clearer picture of the
 // individual for compatibility matching.
 type Metadata interface {
 	// Store iterates over m, saving the values to storage.
 	Store(m MetadataQuery) error
-	// Fetch returns the data requested in m (if present).
-	Fetch(m MetadataQuery) (data /*FIXME*/ interface{}, err error)
+
+	// Fetch ranges over m filling in the values of every key present,
+	// returning the total number of changes made.
+	Fetch(m MetadataQuery) (n int, err error)
 }
 
 // MetadataQuery is used to query, and fetch Client Metadata.
-type MetadataQuery map[string]string
+type MetadataQuery map[string]interface{}
 
-func (mq *MetadataQuery) Store() error {
+func (mq *MetadataQuery) Store(m MetadataQuery) error {
+	for k, v := range m {
+		(*mq)[k] = v
+	}
+	return nil
+}
 
+func (mq *MetadataQuery) Fetch(m MetadataQuery) int {
+	var cnt int
+	for k := range m {
+		if nv, ok := (*mq)[k]; ok {
+			m[k] = nv
+			cnt++
+		}
+	}
+	return cnt
 }
 
 // BaseMetadataQuery is the most basic MetadataQuery, which contains only "last_seen",
 // and "display_name".
 var BaseMetadataQuery = MetadataQuery{"last_seen": "", "display_name": ""}
 
-type authdata struct {
-	client *Client
+type oauthData struct {
+	Client *Client
 
 	// auth data from provider
 	authobj   objx.Map
